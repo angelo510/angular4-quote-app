1ce5dd76ffd8c0525a9def6440bfcd3f35542965
Angelo <re.se.t@outlook.com>
2017-05-24 14:46:01 +0100

    model of movie

---
+++ app/movies/movie.ts
@@ -0,0 +1,10 @@
+export interface Movie{
+	movieId : number;
+	movieName : string;
+	code : string;
+	releaseDate : string;
+	price : number;
+	description : string;
+	imdbRating : number;
+	imageUrl : string;
+}
