1e1defca548cb5393f71a7973911745201edfbef
Angelo <re.se.t@outlook.com>
2017-07-18 19:05:14 +0100

    env for dev and prod

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://django-env.pyanchvv6b.us-east-1.elasticbeanstalk.com/',
+  };
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://django-env.pyanchvv6b.us-east-1.elasticbeanstalk.com/',
+  };
