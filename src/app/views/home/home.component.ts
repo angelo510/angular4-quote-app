a8db502bfd278897c76aa2e1ab10f9f66cc70db9
Angelo <re.se.t@outlook.com>
2017-07-21 19:08:52 +0100

    home service

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { HomeService } from './home.service';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  AfterViewInit
+} from '@angular/core';
+import {
+  ViewChildren,
+  QueryList,
+  ElementRef
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import {
+  HttpErrorResponse
+} from '@angular/common/http';
 
-import { Subject } from 'rxjs/Subject';
-import { debounceTime } from 'rxjs/operator/debounceTime';
+import {
+  Router,
+  ActivatedRoute,
+  Params
+} from '@angular/router';
+import {
+  NgbModal,
+  NgbModalRef
+} from '@ng-bootstrap/ng-bootstrap';
+
+import {
+  Subject
+} from 'rxjs/Subject';
+import {
+  debounceTime
+} from 'rxjs/operator/debounceTime';
+
+import {
+  HomeService
+} from './home.service';
+
+import {
+  SignatureFieldComponent
+} from './signature-field/signature-field.component';
+import {
+  IMyDpOptions
+} from 'mydatepicker';
 
 import {
   IQuote,
@@ -14,6 +54,8 @@ import {
   IAccountInfo,
   IContactInfo
 } from '../../models/quote';
+
+import { INoteData } from '../../models/notes';
 
 @Component({
   selector: 'home',
@@ -26,26 +68,52 @@ import {
 
 export class HomeComponent implements OnInit {
 
+  @ViewChildren('signatureField') public signatureField: SignatureFieldComponent;
+
   private quote: IQuote;
-  private monthlyItems: IQuoteItem[];
-  private oneTimeItems: IQuoteItem[];
-  private isLoading: boolean;
+  private notes: INoteData;
+  private monthly: IQuoteItem;
+  private oneTime: IQuoteItem;
 
   private quoteId: string;
   private accountId: string;
+  private expireDate: string;
 
-  private _success = new Subject<string>();
+  private _success = new Subject < string > ();
 
   private staticAlertClosed = false;
   private successMessage: string;
-
+  private form: FormGroup;
   private modalRef: NgbModalRef;
+
+  private myDatePickerOptions: IMyDpOptions = {
+    dateFormat: 'dd/mm/yyyy',
+    width: '78%',
+    height: '40px',
+    editableDateField: false,
+    openSelectorOnInputClick: true,
+  };
+  private dataPicker: any = {
+    date: {
+      year: 2018,
+      month: 10,
+      day: 9
+    }
+  };
+
+  private isLoading: boolean;
+  private isCollapsed = false;
 
   constructor(
     private homeServce: HomeService,
     private activatedRoute: ActivatedRoute,
     private modalService: NgbModal,
+    private fb: FormBuilder,
+    private router: Router,
   ) {
+    this.form = fb.group({
+      signatureField: ['', Validators.required],
+    });
     this.isLoading = true;
   }
 
@@ -57,27 +125,49 @@ export class HomeComponent implements OnInit {
     });
     this.homeServce.getQuote(this.quoteId, this.accountId)
       .subscribe((q) => {
-        this.quote = q;
-        this.monthlyItems = this.quote.data.quote_items.monthly_items;
-        this.oneTimeItems = this.quote.data.quote_items.one_time_items;
-        this.isLoading = false;
-      });
+          this.quote = q;
+          this.expireDate = this.quote.data.quote.expiration_date.toString();
+          this.monthly = this.quote.data.quote_items.monthly;
+          this.oneTime = this.quote.data.quote_items.one_time;
+          this.isLoading = false;
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log('An error occurred:', err.error.message);
+          } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            this.router.navigateByUrl('/confirm');
+          }
+        }
+      );
+    this.homeServce.getNotes(this.quoteId, this.accountId)
+      .subscribe((notes) => { this.notes = notes; });
 
     this._success.subscribe((message) => this.successMessage = message);
     debounceTime.call(this._success, 5000).subscribe(() => this.successMessage = null);
-
   }
 
-  private OnProceed(content) {
-    this.modalRef = this.modalService.open(content, { size: 'lg' });
+  private OnProceed(content, flag: boolean = true) {
+    if (this.modalRef) {
+      this.modalRef.close();
+    }
+    this.modalRef = this.modalService.open(content, {
+      size: 'lg'
+    });
   }
 
   private OnAccept() {
     this.homeServce.wonQuote(this.quoteId, this.accountId)
-    .subscribe((result) => {
-      this._success.next('The Quote has been won!');
-      this.modalRef.close();
-    });
+      .subscribe((result) => {
+        this._success.next('The Quote has been won!');
+        this.modalRef.close();
+      });
+  }
+
+  private onClear() {
+    console.log(this.signatureField._signature);
+    this.signatureField.clear();
   }
 
 }
--- src/app/views/home/home.service.ts
+++ src/app/views/home/home.service.ts
@@ -20,6 +20,14 @@ export class HomeService {
             .map((response: Response) => response.json());
     }
 
+    public getNotes(quoteId: string, accountId: string) {
+        let params: URLSearchParams = new URLSearchParams();
+        params.set('quote_id', quoteId);
+        params.set('account_id', accountId);
+        return this.http.get(environment.apiUrl + 'quote/notes', {search: params})
+            .map((response: Response) => response.json());
+    }
+
     public wonQuote(quoteId: string, accountId: string) {
         const body = { quote_id: quoteId, account_id: accountId };
         return this.http.put(environment.apiUrl + 'quote/won', body)
