c70b0ea3306e52e1fa2549603efa6c5b8766b467
Angelo <re.se.t@outlook.com>
2017-08-08 02:00:14 +0100

    created sidebar component

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: 'quote', title: 'Quote',  icon: 'dashboard', class: '' },
+    { path: 'template-setting', title: 'Template Setting',  icon: 'settings', class: '' },
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
