7eee6ee2e73386fb8fe534eeffee880255dbff7c
Angelo <re.se.t@outlook.com>
2017-08-14 02:03:10 +0100

    template settings page

---
+++ src/app/template-setting/template-setting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-setting',
+  templateUrl: './template-setting.component.html',
+  styleUrls: ['./template-setting.component.css']
+})
+export class TemplateSettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
